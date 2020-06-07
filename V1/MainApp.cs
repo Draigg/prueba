7abec264b664359b48cfc971294c5d95693c27db
Julio Rodriguez <r.julio115@gmail.com>
2020-06-07 17:08:42 -0600

    Se agrego V1 en feature sin patrones

---
+++ V1/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+        string path;
+        string line2;
+        List<string> lines;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                path = @"..\..\..\" + args[i];
+                lines = new List<string>();
+                lines = File.ReadAllLines(path).ToList();
+                foreach (String line in lines)
+                {
+                    line2 = line.Replace(' ', ',');
+                    Console.WriteLine(line2);
+                }
+            }
+
+        }
+    }
+}
